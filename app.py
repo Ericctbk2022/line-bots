db7921b3c101a61ea8b63fac19037960a19c8a78
EricChen <ericctbk2022@gmail.com>
2024-08-25 18:07:44 +0800

    add some deploy to render

--- app.py
+++ app.py
@@ -20,39 +20,61 @@ from linebot.v3.webhooks import (
 
 app = Flask(__name__)
 
-configuration = Configuration(access_token='7ye5TfCfI5OUe/lmpRzU4lRdqnHU2iZvPF4dEoz7nKesgUOyW6fB2yenLR1GL6obS3NOg4ZOGk/6Bwc3vQyGCluHS0xHJmuXJGSr0zBRwADPVpXSlodqbESVHCdFu2j4MsXw68cEuRXq1xuKGphLyQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('57621dba7b714219dc8da61260373339')
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('57621dba7b714219dc8da61260373339', None)
+channel_access_token = os.getenv('7ye5TfCfI5OUe/lmpRzU4lRdqnHU2iZvPF4dEoz7nKesgUOyW6fB2yenLR1GL6obS3NOg4ZOGk/6Bwc3vQyGCluHS0xHJmuXJGSr0zBRwADPVpXSlodqbESVHCdFu2j4MsXw68cEuRXq1xuKGphLyQdB04t89/1O/w1cDnyilFU=', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+parser = WebhookParser(channel_secret)
+
+configuration = Configuration(
+    access_token=channel_access_token
+)
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    # parse webhook body
     try:
-        handler.handle(body, signature)
+        events = parser.parse(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessageContent):
+            continue
+        with ApiClient(configuration) as api_client:
+            line_bot_api = MessagingApi(api_client)
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text=event.message.text)]
+                )
+            )
 
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessageContent)
-def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
-        )
-
 if __name__ == "__main__":
-    app.run()
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, port=options.port)
