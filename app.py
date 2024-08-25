60b300c84b512695fadc9bcadb55671dabbd0228
EricChen <ericctbk2022@gmail.com>
2024-08-25 22:02:22 +0800

    fix show log error

--- app.py
+++ app.py
@@ -46,12 +46,11 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info('Request body: ' + body)
+    app.logger.info("Request body: " + body)
 
     # parse webhook body
     try:
         events = parser.parse(body, signature)
-        print("events -> " +events)
     except InvalidSignatureError:
         abort(400)
 
